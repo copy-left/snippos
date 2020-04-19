8c2253cddf76627f35795a8d6f0c4dfa27dd8fb1
Midhun Mohan <midhunvaikom@gmail.com>
2020-04-19 13:47:05 +0530

    cleaning up

--- src/main/java/com/copyleft/snippos/SnipposApplication.java
+++ src/main/java/com/copyleft/snippos/SnipposApplication.java
@@ -2,11 +2,8 @@ package com.copyleft.snippos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({LiquibaseProperties.class})
 public class SnipposApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/copyleft/snippos/repository/UserRepository.java
+++ src/main/java/com/copyleft/snippos/repository/UserRepository.java
@@ -4,8 +4,6 @@ import com.copyleft.snippos.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
